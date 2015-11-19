8cd96956ff899f76614c32ff7be0f6858cb23196
MarcelGehrig2 <marcelgehrig90@hotmial.com>
2015-11-19 23:37:36 +0100

    Problem003: created

---
+++ Problem003/src/Main.java
@@ -0,0 +1,42 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Largest prime factor
+		// ////////////////////
+		
+		/*
+		 * The prime factors of 13195 are 5, 7, 13 and 29.
+		 * What is the largest prime factor of the number 600851475143 ?
+		 */
+		
+		// # of attempts:	
+		// success:			false
+		
+		
+//		int fib_0 = 0;
+//		int fib_1 = 1;	// init
+//		int fib_2 = 0;
+//		
+//		int sumEvenFib = 0;
+//		
+//		while ((fib_1 + fib_2) < 4e6) {
+//			fib_0 = fib_1 + fib_2;	// calculating new value
+//			
+////			System.out.print("Fibonacci number = " + fib_0);
+//			
+//			if ( (fib_0 % 2) == 0 )	sumEvenFib += fib_0; 
+//			
+//			// shifting Variables
+//			fib_2 = fib_1;
+//			fib_1 = fib_0;
+//			
+////			System.out.println();
+//		};
+//		
+//		System.out.println();
+//		System.out.println("The sum of all even Values is: " + sumEvenFib);
+		
+	}
+
+}
