a0488b5cfddffd125b263c17bddd04e4827a9a5c
MarcelGehrig2 <marcelgehrig90@hotmial.com>
2015-11-20 18:41:17 +0100

    Problem001: Elapsed time measuring added

--- Problem001/src/Main.java
+++ Problem001/src/Main.java
@@ -10,26 +10,34 @@ public class Main {
 		 * Find the sum of all the multiples of 3 or 5 below 1000.
 		 */
 		
-		// # of attempts:	3
-		// success:			true
+		// # of attempts:		3
+		// success:				true
+		// calculating time:	35.7usec	(i < 1000)
+		
+		long startTime = System.nanoTime();
+		
 		
 		int sum = 0;
 		
 		for (int i=0; i < 1000; i++) {
-			System.out.print("i = " + i + ",   ");
+//			System.out.print("i = " + i + ",   ");
 			if ( (i % 3 == 0) && (i != 0) ) {
 				sum += i;
-				System.out.print(",    sum3 = " + sum);
+//				System.out.print(",    sum3 = " + sum);
 			}
 			else if ( (i % 5 == 0) && (i != 0) ) {
 				sum += i;
-				System.out.print(",    sum5 = " + sum);
+//				System.out.print(",    sum5 = " + sum);
 			}
-			System.out.println();
+//			System.out.println();
 		}
+		
+		
+		long stopTime = System.nanoTime();
 		
 		System.out.println();
 		System.out.println("The total sum is: " + sum);
+	System.out.println("Calculating time: " + ((float)(stopTime-startTime)/1000) + "usec");
 	}
 
 }
