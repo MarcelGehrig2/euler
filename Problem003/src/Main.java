a4e2f82696c02f45d9edc3f2300c22f25072ba9d
MarcelGehrig2 <marcelgehrig90@hotmial.com>
2015-12-19 17:06:05 +0100

    Problem003 		// # of attempts:		1 		// success:				true 		// calculating time:	1223  usec 		// answer:				6857

--- Problem003/src/Main.java
+++ Problem003/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Main {
 
@@ -10,10 +11,36 @@ public class Main {
 		 * What is the largest prime factor of the number 600851475143 ?
 		 */
 		
-		// # of attempts:	
-		// success:			false
+		// # of attempts:		1
+		// success:				true
+		// calculating time:	1223  usec
+		// answer:				6857
+		
+
+		long inputNumber;
+		inputNumber = 600851475143L;
+		//inputNumber = 13195;
+		//inputNumber = 60;
 		
 		
+		
+		// CODE
+		long startTime = System.nanoTime();
+		
+		
+		ArrayList<Long> primeNumbers = new ArrayList<Long>();
+		long inputNumberCopy = inputNumber;
+		
+		for (long i=2L; i<= inputNumberCopy; i++) {
+			if (divideable(inputNumberCopy, i)) {
+				primeNumbers.add(i);
+				inputNumberCopy = inputNumberCopy / i;
+				i=1;
+			};
+		}
+		
+		
+		long largestPrime = primeNumbers.get(primeNumbers.size()-1);
 //		int fib_0 = 0;
 //		int fib_1 = 1;	// init
 //		int fib_2 = 0;
@@ -37,6 +64,23 @@ public class Main {
 //		System.out.println();
 //		System.out.println("The sum of all even Values is: " + sumEvenFib);
 		
+		
+		
+		// RESULTS
+		long stopTime = System.nanoTime();
+
+		System.out.println();
+		System.out.println("The prime factors are:\t\t" + primeNumbers);
+		System.out.println("The largest primefactor is:\t" + largestPrime);
+		System.out.println("Calculating time:\t\t" + ((int)(stopTime-startTime)/(1000)) + " usec");
+		
+	}
+	
+	public static Boolean divideable(long divident, long divisor) {
+		if (divident % divisor == 0) {
+			return true;
+		}
+		else return false;
 	}
 
 }
