aa4c1f5b9fe21ebdeea0291f73d58f3226884f75
MarcelGehrig2 <marcelgehrig90@hotmial.com>
2015-12-21 17:54:54 +0100

    Problem004 solved

--- Problem004/src/Main.java
+++ Problem004/src/Main.java
@@ -11,17 +11,40 @@ public class Main {
 		 * Find the largest palindrome made from the product of two 3-digit numbers.
 		 */
 		
-		// # of attempts:		0
-		// success:				
-		// calculating time:	 usec
-		// answer:				
+		// # of attempts:		3
+		// success:				true
+		// calculating time A:	11211 usec
+		// answer:				906609
 		
 
 		// CODE
 		long startTime = System.nanoTime();
 		
 		
-//		ArrayList<Long> primeNumbers = new ArrayList<Long>();
+		int largestFactor = 999;
+		int largestPalindrom = 0;
+//		Boolean largestPalindromIsFound = false;
+		int n1=0;
+		int n2=0;
+		
+		for (n1=largestFactor; n1>=0; n1--) {
+			for (n2=largestFactor; n2>=n1; n2--) {
+				
+//				System.out.println("n1: " + n1);
+//				System.out.println("n2: " + n2);
+				
+				int product = n1*n2;
+				if (isPalindrome(product)) {
+//					largestPalindromIsFound = true;
+					if (largestPalindrom < product) largestPalindrom = product;
+//					break;
+				}
+			}
+//			if (largestPalindromIsFound) break;
+		}
+		
+		
+		
 //		long inputNumberCopy = inputNumber;
 //		
 //		for (long i=2L; i<= inputNumberCopy; i++) {
@@ -41,10 +64,72 @@ public class Main {
 		long stopTime = System.nanoTime();
 
 		System.out.println();
-//		System.out.println("The prime factors are:\t\t" + primeNumbers);
-//		System.out.println("The largest primefactor is:\t" + largestPrime);
+		System.out.println("The largest palindrome is:\t" + largestPalindrom);
+		System.out.println("Factor 1:\t\t\t" + n1);
+		System.out.println("Factor 2:\t\t\t" + n2);
 		System.out.println("Calculating time:\t\t" + ((int)(stopTime-startTime)/(1000)) + " usec");
 		
 	}
 
+	
+	public static Boolean isPalindrome(int n) {
+		int nCopy = n;
+		Boolean isPalindrome = true;
+		
+//		if ((n>=100000) && (n<=999999)) {
+			if (n/100000 != n%10) {
+				isPalindrome = false;
+			}
+			n = (n%100000)/10;		// cuts first and last digit
+			
+//			System.out.println("First cut: " + n);
+//		}
+		
+//		if ((n>=100) && (n<=9999)) {
+			if (n/1000 != n%10) {
+				isPalindrome = false;
+			}
+			n = (n%1000)/10;		// cuts first and last digit
+
+//			System.out.println("Second cut: " + n);
+//		}
+		
+//		if ((n>=1) && (n<=99)) {
+			if (n/10 != n%10) {
+				isPalindrome = false;
+			}
+//		}
+		
+//		System.out.println("is palindrome: " + nCopy + "   " + isPalindrome);
+		return isPalindrome;
+	}
+
+	
+	public static Boolean isPalindromeB(int n) {
+		int nCopy = n;
+		Boolean isPalindrome = true;
+		String strN = Integer.toString(n);
+		
+		if (strN.charAt(0) != strN.charAt(strN.length()-1)) {
+			isPalindrome = false;
+		}
+		
+		strN = strN.substring(1, strN.length()-1);
+		System.out.println("First cut: " + strN);
+		
+		if (strN.charAt(0) != strN.charAt(strN.length()-1)) {
+			isPalindrome = false;
+		}
+		
+		strN = strN.substring(1, strN.length()-1);
+		System.out.println("Second cut: " + strN);
+		
+		if (strN.charAt(0) != strN.charAt(strN.length()-1)) {
+			isPalindrome = false;
+		}
+		
+		
+		System.out.println("is palindrome: " + nCopy + "   " + isPalindrome);
+		return isPalindrome;
+	}
 }
