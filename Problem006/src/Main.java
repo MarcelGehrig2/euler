a0e35c9a5dd8ee7fa8b92a0a6885b4b448b03ff8
MarcelGehrig2 <marcelgehrig90@hotmial.com>
2015-12-22 17:54:02 +0100

    Problem006: recreated

---
+++ Problem006/src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// (004) Largest palindrome product
+		// ////////////////////////////////
+		
+		/*
+	 	 * The sum of the squares of the first ten natural numbers is,
+	 	 * 12 + 22 + ... + 102 = 385
+	 	 * 
+	 	 * The square of the sum of the first ten natural numbers is,
+	 	 * (1 + 2 + ... + 10)2 = 552 = 3025
+	 	 * 
+	 	 * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640.
+	 	 * 
+	 	 * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+		 */
+		
+		// # of attempts:		
+		// success:				
+		// calculating time :	
+		// answer:				
+		
+
+		// CODE
+		long startTime = System.nanoTime();
+		int answer = 0;
+		
+	
+		
+
+		// RESULTS
+		long stopTime = System.nanoTime();
+
+		System.out.println();
+		System.out.println("The difference is:\t" + answer);
+		System.out.println("Calculating time:\t\t" + ((int)(stopTime-startTime)/(1000)) + " usec");
+		
+	}
+	
+}
